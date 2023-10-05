842a18a04433ef416daede8054ae2b3e9573a248
Miguel Angel Ruz <miguelangelruz@192.168.1.8>
2023-10-04 23:02:56 -0500

    Ajustes varios

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ const ejs = require('ejs');
 const app = express();
 const port = 3000;
 
-// Configura el motor de plantillas para renderizar vistas HTML
-app.set('view engine', 'ejs'); // Puedes usar EJS u otro motor de plantillas
+// Configuración del motor de plantillas para renderizar vistas HTML
+app.set('view engine', 'ejs'); 
 
-// Establece la ubicación de tus archivos de vista
+// Ubicación de archivos de vista
 app.set('views', __dirname);
 
 //servir archivos estaticos
-app.use(express.static('public')); // 'public' es el nombre de tu carpeta de archivos estáticos
+app.use(express.static('public'));
 
 
-// Configura el motor de plantillas EJS
+// Configuración el motor de plantillas EJS
 app.set('view engine', 'ejs');
 
-// Define tus rutas y lógica de manejo aquí
+//Rutas y lógica de manejo
 // Ruta de inicio
 app.get('/', (req, res) => {
     res.render('index.ejs');
